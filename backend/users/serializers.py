b2a34680a2386bb57506cba1041fe45886596b9b
Yash Sharma <yshsharma90913@gmail.com>
2026-05-24 21:24:50 +0530

    added user profile and list views

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,11 @@
 from rest_framework import serializers
 from .models import User
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer, UserListSerializer
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -21,3 +27,30 @@ class UserListSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'name', 'avatar']
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_profile(request):
+    """logged in user ki profile return karo"""
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_profile(request):
+    """user apna naam ya avatar update kar sake"""
+    serializer = UserSerializer(request.user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_users(request):
+    """task assign karne ke liye saare users ki list"""
+    users = User.objects.exclude(id=request.user.id)
+    serializer = UserListSerializer(users, many=True)
+    return Response(serializer.data)
