9c0eef07a8f9f1ab5f9ea519c7473a33ac0e0a86
Yash Sharma <yshsharma90913@gmail.com>
2026-05-24 21:48:06 +0530

    added user profiles and list views

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer, UserListSerializer
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def get_profile(request):
+    
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def update_profile(request):
+    serializer = UserSerializer(request.user, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def list_users(request):
+    users = User.objects.exclude(id=request.user.id)
+    serializer = UserListSerializer(users, many=True)
+    return Response(serializer.data)
