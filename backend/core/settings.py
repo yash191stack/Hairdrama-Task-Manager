b982d81a6d64d8725207979120a025874d693e03
Yash Sharma <yshsharma90913@gmail.com>
2026-05-27 18:01:26 +0530

    allowed all hosts to fix cors error

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
